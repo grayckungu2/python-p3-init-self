8aac8aa91ab49bbbf726655a50640818cfb3f7d1
Grace kungu <grayckungu2@gmail.com>
2023-08-23 09:13:31 +0300

    complted assignment

---
+++ lib/main .py
@@ -0,0 +1,12 @@
+from dog import Dog
+from person import Person
+
+dog = Dog("Buddy")
+print(dog.name)  
+print(dog.breed)  
+dog.sit()
+
+person = Person("kui")
+print(person.name)  
+person.talk()
+person.walk()
